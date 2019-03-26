3cb1485ec206c00c2d48528cd3d5ae44c29affaf
Taiga Kiyotaki <t2421gm@gmail.com>
2019-03-26 14:04:33 +0900

    lib-RxSwift 追加

---
+++ lib-RxSwift/Pods/Target Support Files/Pods-lib-RxSwiftTests/Pods-lib-RxSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lib_RxSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_lib_RxSwiftTests
+@end
---
+++ lib-RxSwift/lib-RxSwiftTests/lib_RxSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  lib_RxSwiftTests.swift
+//  lib-RxSwiftTests
+//
+//  Created by taigakiyotaki on 2019/03/26.
+//  Copyright © 2019 taigakiyotaki. All rights reserved.
+//
+
+import XCTest
+@testable import lib_RxSwift
+
+class lib_RxSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
